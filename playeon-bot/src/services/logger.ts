96899abec27c867d5206369ab451f52c912e18fd
Dipu <sadhanaroy198421@gmail.com>
2026-09-21 23:19:41 +0530

    Update logger.ts

--- playeon-bot/src/services/logger.ts
+++ playeon-bot/src/services/logger.ts
@@ -1,12 +1,3 @@
-import { logUserStarted } from './logs/userStarted.js'
-import { logBotJoinedChat } from './logs/botJoinedChat.js'
-import { logBotLeftChat } from './logs/botLeftChat.js'
-import { logPrefixChanged } from './logs/prefixChanged.js'
-import { logSuperuserAdded } from './logs/superuserAdded.js'
-import { logSuperuserRemoved } from './logs/superuserRemoved.js'
-import { logTrackPlayed } from './logs/trackPlayed.js'
-import { logCommandError } from './logs/commandError.js'
-
 export type Logger = typeof logger
 
 export const logger = {
